6aa6474e15f75acd761cecbe182a5f0528dfb9a3
nitin7213 <nitineon123@gmail.com>
2024-06-01 22:41:32 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+//Apollo Imports
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+
+//APIS
+const { USERS } = require("./user");
+const { TODOS } = require("./todo");
+
+const startServer = async () => {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs: `
+        type User {
+            id: ID!
+            name: String!
+            username: String!
+            email: String!
+            phone: String!
+            website: String!
+        }
+
+        type Todo {
+            id: ID!
+            title: String!
+            completed: Boolean
+            user: User
+        }
+
+        type Query {
+            getTodos: [Todo]
+            getAllUsers: [User]
+            getUser(id: ID!): User
+        }
+
+    `,
+    resolvers: {
+      Todo: {
+        user: (todo) => USERS.find((e) => e.id === todo.id),
+      },
+      Query: {
+        getTodos: () => TODOS,
+        getAllUsers: () => USERS,
+        getUser: async (parent, { id }) => USERS.find((e) => e.id === id),
+      },
+    },
+    debug: true,
+  });
+
+  app.use(bodyParser.json());
+  app.use(cors());
+
+  await server.start();
+
+  app.use("/graphql", expressMiddleware(server));
+
+  app.listen(8000, () => console.log("Serevr Started at PORT 8000"));
+};
+
+startServer();
